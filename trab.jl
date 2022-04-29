afa75e199addf84dc6e17a975cbdc26f03fb8e3a
arturrossi <artur_rossi@hotmail.com>
2022-04-28 23:40:52 -0300

    wip: retorna infactivel em sempre

--- trab.jl
+++ trab.jl
@@ -1,25 +1,31 @@
 using JuMP
 using GLPK
+using DelimitedFiles
 
-M = 1000000 #constante
+#ARRAY EM JULIA COMECA NO INDICE 1 :)
 
-n = 10 #numero de vertices
+instancia = readdlm("instancias-problema2/instance_14.dat", ' ')
+
+M = 1000 #constante
+
+n = instancia[1] #numero de vertices
 S = (2^n) - 2 #numero subconjuntos que nao sao vazios nem V
 
-a = n * (n - 1) / 2 #numero de arestas
-
-d = rand(10:100, n, n) #distancias
+d = instancia[2:n+1, 1:n] #distancias
 
 #Cada vertice é um cliente e tem
-D = rand(100:1000, n) #D(v) demanda
-l = rand(100:1000, n) #L(v) limite
+D = instancia[n+2, 1:n] #D(v) demanda
+l = instancia[n+3, 1:n]  #L(v) limite
 
 model = Model(GLPK.Optimizer)
 
 #Xij, 1 se aresta ij faz parte da solucao
 @variable(model, x[1:n, 1:n], Bin)
+
+c_initial = fill(sum(D[1:n]), n)
+# c_initial[1] = sum(D[1:n])
 #Cv, quantidade restante de um produto no vertice
-@variable(model, c[1:n])
+@variable(model, c[i=1:n], start = c_initial[i])
 
 #funcao objetivo
 @objective(model, Min,
@@ -28,11 +34,14 @@ model = Model(GLPK.Optimizer)
 
 #RESTRICOES
 
-#suprir a demanda dos clientes
-@constraint(model, demanda[i=1:n], sum(d[i]) == c[0])
+#retirar subciclos
+@constraint(model, sum(x[1:n, 1:n]) <= S - 1)
 
-#carga maxima (carga c tem que ser no maximo l)
-@constraint(model, carga_max[v=1:n], l[v] >= c[v])
+#restricao de grau 2 (exatamente uma aresta entrando e exatamente uma aresta saindo do vertice)
+@constraint(model, restr[i=1:n, j=1:n], sum(x[i, j]) + sum(x[j, i]) == 2)
+
+#suprir a demanda dos clientes
+@constraint(model, sum(D[1:n]) == c[1])
 
 #se aresta está na solução, 
 #somatorio das cargas restantes tem que ser no máximo igual a carga
@@ -44,11 +53,10 @@ model = Model(GLPK.Optimizer)
 #nos outros vértices
 @constraint(model, restante2[i=1:n, v=2:n], sum(c[i] - D[i]) >= c[v] + M * (1 - x[i, v]))
 
-#restricao de grau 2 (exatamente uma aresta entrando e exatamente uma aresta saindo do vertice)
-@constraint(model, restr[i=1:n, j=1:n], sum(x[i, j]) + sum(x[j, i]) == 2)
+#carga maxima (carga c tem que ser no maximo l)
+@constraint(model, carga_max[v=1:n], l[v] >= c[v])
 
-#retirar subciclos
-@constraint(model, restr2[i=1:n, j=1:n], sum(x[i, j] <= S - 1))
+# println(model)
 
 optimize!(model)
 
@@ -58,4 +66,5 @@ if termination_status(model) == MOI.OPTIMAL
     @show value.(x)
 else
     println("Infactivel ou ilimitado")
+    println(termination_status(model))
 end
